c06ab64775feb73fc94e12ed27c3d1321fc474ca
Sarah Jacob <smjacob91@gmail.com>
2018-07-04 16:22:31 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,20 +39,11 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  array
-
-
-
-
-
-describe "#using_delete" do 
-  it "takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string" do
-    instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    no_offense_steven = using_delete(instructors, "Steven")
-    expect(instructors).not_to include("Steven")
-  end
+  array.delete(string)
 end
 
+def using_delete_at(array, integer)
+  
 describe "#using_delete_at" do 
   it "takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer" do 
     famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
