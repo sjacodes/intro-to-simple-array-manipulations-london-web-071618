c64285aff5b90b6952216687e4280e1ba3cf7f4f
Sarah Jacob <smjacob91@gmail.com>
2018-07-04 16:13:35 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,4 +14,35 @@ def pop_with_args(array)
   array.pop(2)
 end
 
+def using_shift(array)
+  
 
+
+describe "#using_shift" do 
+  before(:each) do 
+    @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+    @im_so_over_this_city = using_shift(@my_favorite_cities)
+  end
+  it "takes in an argument of an array and uses the shift method to remove the first item and return it" do 
+    expect(@im_so_over_this_city).to eq("Lagos")
+  end
+
+  it "decreases the length of the array by 1" do 
+    expect(@my_favorite_cities.size).to eq(7)
+  end
+end
+
+describe "#shift_with_args" do 
+  before(:each) do 
+    @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+    @brands_removed = shift_with_args(@ice_cream_brands)
+  end
+  
+  it "takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array" do 
+    expect(@brands_removed).to eq(["Blue Bell Creameries", "Ben & Jerry's"])
+  end
+
+  it "decreases the length of the array by 2" do 
+    expect(@brands_removed.size).to eq(2)
+  end
+end
